3b4db1e629d63ac9c0ca65e4c2b7d9703ea93099
Kevin Cotrone <kevincotrone@gmail.com>
2015-03-13 17:08:32 -0500

    Using requestAnimationFrame and added an actuall tank value

--- examples/square.hs
+++ examples/square.hs
@@ -10,33 +10,58 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 import GHCJS.Foreign.QQ
 
+import Data.Text
+import Text.Printf
+
 newtype Scale = Scale { unScale :: (Int, Int) } deriving (Eq, Show)
+
 
 scaleWidthHeight :: Scale -> (Int,Int) -> (Int, Int)
 scaleWidthHeight (Scale (absWidth, absHeight)) (width, height) = (floor width' , floor height')
   where width' = (fromIntegral $ width * absWidth) / 1000.0 :: Double
         height' = (fromIntegral $ height * absHeight )/ 1000.0 :: Double
 main = do
-  let sc = Scale (200,200)
   ctx <- getContext =<< indexArray 0 . castRef =<< select "#theCanvas"
   i <- createImage "test.png"
   g <- createImage "animation-tankBlue.png"
   let sc = Scale (200,200)
   let render = drawTank ctx i g
-  mapM_ (\ p -> render p >> threadDelay 1000000 >> clearRect 0 0 124 200 ctx ) $ cycle [0.0,0.1..1.0]
+  mapM_ (\ p -> onAnimation $ clearRect 0 0 2000 2000 ctx >> render 200 100 (easeIn 1.0 p)) $ [0.0,0.02..1.0]
 
 
-drawTank :: Context -> Image -> Image -> Double -> IO ()
-drawTank ctx i g fullPercentage = do
+
+easeIn :: Double -> Double -> Double
+easeIn maxP t = maxP * if t < 0.5 
+                         then 2*t*t 
+                         else -1+(4-2*t)*t
+
+
+drawTank :: Context -> Image -> Image -> Double -> Double -> Double -> IO ()
+drawTank ctx i g maxVal val progress = do
+  let fullPercentage = progress * (val / maxVal)
+      currentAnimVal = progress * val
   (width, height) <- imageDimensions g
   drawImage i 0 0 124 200 ctx
   drawImagePercentage ctx g (124,200) fullPercentage
   drawLines ctx
-  translate 0 (185 - 180 * fullPercentage) ctx
+  translate 0 (190 - 180 * fullPercentage) ctx
   drawCenterLine ctx
   translate 155 0 ctx
   drawTriangle ctx
+  drawPrecentage ctx currentAnimVal
+  translate (-155) 0 ctx
+  translate 0 (- (190 - 180 * fullPercentage)) ctx
   return ()
+
+
+drawPrecentage :: Context -> Double -> IO ()
+drawPrecentage ctx val = do
+  let dx = 10
+      dy = 4
+  translate  dx dy ctx
+  font "12px Georgia" ctx
+  fillText (pack $ printf "%.2f\n" (val)) 0 0 ctx
+  translate (-dx) (-dy) ctx
 
 
 -- | Given an image, (width, height) and the height percentage,
@@ -91,7 +116,7 @@ drawLines ctx = do
   lineWidth 1 ctx
   translate 150 10 ctx
   mapM_ makeTick [0,18..180]
-  translate (-150) (-5) ctx
+  translate (-150) (-10) ctx
   where makeTick :: Double -> IO ()
         makeTick y = do
           beginPath ctx
@@ -99,6 +124,14 @@ drawLines ctx = do
           lineTo 5 (y) ctx
           stroke ctx
           closePath ctx
+
+
+
+-----------------------------------------------------
+-- Below are functions that I needed that aren't in
+-- the canvas library right now
+-----------------------------------------------------
+
 
 
 -- | Look at https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing for more
@@ -118,6 +151,14 @@ imageHeight img = [js|`img.height|]
 imageWidth :: Image -> IO Double
 imageWidth img = [js|`img.width|]
 
+
+onAnimation :: IO () -> IO ()
+onAnimation action = do
+  mv <- newEmptyMVar
+  cb <- syncCallback AlwaysRetain True (action >> putMVar mv ())
+  [js_|window.requestAnimationFrame(`cb)|]
+  takeMVar mv
+
 -- | Load an image into a JSRef wrapper
 -- and wait for the image to be loaded
 createImage :: String -> IO Image
@@ -132,108 +173,3 @@ createImage imageName = do
 
 newImage :: IO Image
 newImage = [js| new Image() |]
-
--- displayGauge :: Context -> Double -> IO ()
--- displayGauge ctx height = do
---   translate 20 20 ctx
---   drawInside ctx 130
---   drawOutline ctx
---   threadDelay 1000000 -- do
-  -- let scaleWH = scaleWidthHeight sc
-  --     scaleW = \w -> fst $ scaleWidthHeight sc (w,0)
-  --     scaleH = \h -> snd $ scaleWidthHeight sc (0,h)
-  -- save ctx
-  -- fillStyle 255 255 255 1 ctx
-  -- restore ctx
-  -- save ctx
-  -- -- fillRect (-50) (20) 50 100 ctx
-  -- scale 2 1 ctx
-  -- beginPath ctx
-  -- arc 0 65 30 0 ( 2 * pi) False ctx
-  -- restore ctx
-  -- -- fillStyle 142 214 255 1 ctx
-  -- -- fill ctx
-  -- lineWidth 2 ctx
-  -- stroke ctx
-  -- threadDelay 100000
-
-
--- drawFill :: Double -> Context -> IO ()
--- drawFill height ctx = do
---   globalAlpha 0.9 ctx
---   drawInside ctx height
-
--- drawInside :: Context -> Double -> IO ()
--- drawInside ctx height = do
---   beginPath ctx
---   translate 0 (161 - height) ctx
---   fillStyle 0 0 0 1 ctx
---   fillRect 0 0 100 height ctx
---   closePath ctx
---   translate 0 (- (161 - height)) ctx
-
--- drawOutline :: Context -> IO ()
--- drawOutline ctx = do
---   beginPath ctx
---   fillStyle 255 255 255 0.6 ctx
---   lineWidth 2 ctx
---   strokeRect 0 0 100 161 ctx
---   closePath ctx
---   beginPath ctx
---   drawTicks ctx (100,161) (-5)
---   translate (-100) 0 ctx
---   drawTicks ctx (100,161) (5)
---   beginPath ctx
-
-
-
-
-
-
--- drawTicks :: Context -> (Double, Double) -> Double -> IO ()
--- drawTicks ctx (x,y) tickLength = mapM_ (ticks tickLength) [(x,y/4),(x,y/2),(x,(3 * y / 4))]
---   where ticks :: Double -> (Double, Double) -> IO ()
---         ticks tickLength (x,y) = do
---           beginPath ctx 
---           moveTo x y ctx
---           lineTo (x + tickLength) y ctx
---           lineWidth 2 ctx
---           stroke ctx
-
-
--- drawLine :: Context -> Double -> (Double, Double) -> IO ()
--- drawLine ctx tickLength (x,y) = do
---   beginPath ctx
---   moveTo 0 0 ctx
---   lineTo x y ctx
---   lineWidth 2 ctx
---   stroke ctx
---   mapM_ (leftTick tickLength) [(x,y/4),(x,y/2),(x,(3 * y / 4))]
---   where leftTick :: Double -> (Double, Double) -> IO ()
---         leftTick tickLength (x,y) = do
---           beginPath ctx 
---           moveTo x y ctx
---           lineTo (x + tickLength) y ctx
---           lineWidth 1 ctx
---           stroke ctx
-
-
--- drawBottom :: Context -> IO ()
--- drawBottom ctx = do
---   beginPath ctx
---   save ctx
---   fillStyle 255 255 255 1 ctx
---   restore ctx
---   save ctx
---   scale 2 1 ctx
---   beginPath ctx
---   arc 0 0 30 0 ( 2 * pi) False ctx
---   restore ctx
---   lineWidth 2 ctx
---   stroke ctx
-
--- data TankGauge = TankGauge {
---   tankGaugeMaximum :: Int
--- , tankGaugeValue :: Int
--- } deriving (Eq, Show)
-
